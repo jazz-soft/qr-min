8618d360b67e0410cc1f27f1def274cc49b42ef4
Sema <jazz-dev@jazz-soft.net>
2025-12-14 23:27:56 -0500

    _eyes()

--- javascript/QR.js
+++ javascript/QR.js
@@ -16,15 +16,29 @@
     [1, 1, 1, 1, 1, 1, 1, 0], [1, 0, 0, 0, 0, 0, 1, 0], [1, 0, 1, 1, 1, 0, 1, 0], [1, 0, 1, 1, 1, 0, 1, 0],
     [1, 0, 1, 1, 1, 0, 1, 0], [1, 0, 0, 0, 0, 0, 1, 0], [1, 1, 1, 1, 1, 1, 1, 0], [0, 0, 0, 0, 0, 0, 0, 0]
   ];
-  var QR = function(str) {
-    var v = 1;
-    var sz = v * 4 + 17;
-    var dots = _dots(sz);
-    _static(dots, sz);
+  var _eye = [[1, 1, 1, 1, 1], [1, 0, 0, 0, 1], [1, 0, 1, 0, 1], [1, 0, 0, 0, 1], [1, 1, 1, 1, 1]];
+  var QR = function(n) {
+    // s = new TextEncoder().encode(s);
+    var v = n;
+    var dots = _dots(v);
+    _static(dots, v);
     return dots;
   };
-  function _static(d, sz) {
-    var i, j;
+  function _eyes(v) {
+    var e = [];
+    if (v < 2) return e;
+    e.push(6);
+    var n = Math.floor(v / 7) + 2;
+    var m = v * 4 + 10;
+    var t = Math.floor((v * 8 + n * 3 + 5) / (n * 4 - 4)) * 2;
+    for (var i = 0; i < n - 1; i++) {
+      e.splice(1, 0, m);
+      m -= t;
+    }
+    return e;
+  }
+  function _static(d, v) {
+    var i, j, k, m, sz = v * 4 + 17, e = _eyes(v);
     for (i = 0; i < 8; i++) for (j = 0; j < 8; j++) {
       d[i][j] = _crn[i][j];
       d[i][sz - j - 1] = _crn[i][j];
@@ -36,9 +50,17 @@
       d[6][i] = j;
       j = 1 - j;
     }
+    for (k = 0; k < e.length; k++) for (m = 0; m < e.length; m++) {
+      if (!k && !m) continue;
+      if (!k && m == e.length - 1) continue;
+      if (!m && k == e.length - 1) continue;
+      for (i = 0; i < 5; i++) for (j = 0; j < 5; j++) {
+        d[e[k] + i - 2][e[m] + j - 2] = _eye[i][j];
+      }
+    }
   }
-  function _dots(sz) {
-    var i, d = [];
+  function _dots(v) {
+    var i, d = [], sz = v * 4 + 17;
     for (i = 0; i < sz; i++) d.push(Array(sz).fill(0));
     return d;
   }
--- qr-txt.js
+++ qr-txt.js
@@ -1,4 +1,6 @@
-var qr = require('.')('test');
+const QR = require('.');
+const txt = process.argv[2];
+const qr = QR(txt);
 var i, j, s = '';
 for (i = 0; i < qr.length; i += 2) {
     for (j = 0; j < qr.length; j++) s += i + 1 < qr.length && qr[i + 1][j] ? qr[i][j] ? '█' : '▄' : qr[i][j] ? '▀' : ' ';
